caffe25da4ae69e15cdd48e4df0f3e129b004238
ASH <25776660+iash8090@users.noreply.github.com>
2023-08-27 17:34:38 +0530

    Update Navbar.js

--- src/Componants/Navbar.js
+++ src/Componants/Navbar.js
@@ -1,4 +1,5 @@
 import propTypes from "prop-types";
+import {Link} from 'react-router-dom'
 
 // fixing the datatype of props | errors will be shown on console
 Navbar.propTypes = {
@@ -16,27 +17,32 @@ export default function Navbar(props) {
         <>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="/">
+                    <Link className="navbar-brand" to="/">
                         {" "}
                         {props.title}{" "}
-                    </a>
+                    </Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="/">
+                                <Link className="nav-link active" aria-current="page" to="/">
                                     Home
-                                </a>
+                                </Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="/">
+                                <Link className="nav-link" to="/about">
                                     {props.about_us}
-                                </a>
+                                </Link>
                             </li>
                         </ul>
-
+                        <div className="form-check form-switch mx-2 text-light" style={{ float: "right" }}>
+                            <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+                            <label className="form-check-label" htmlFor="flexSwitchCheckDefault">
+                                <b>Dark Mode</b>
+                            </label>
+                        </div>
                     </div>
                 </div>
             </nav>
