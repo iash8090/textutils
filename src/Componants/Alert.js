17a3f47799d31564fcc4f5f78be84b9fb8b846aa
ASH <25776660+iash8090@users.noreply.github.com>
2023-08-27 17:34:17 +0530

    Update Alert.js

--- src/Componants/Alert.js
+++ src/Componants/Alert.js
@@ -1,13 +1,19 @@
 import React from "react";
 
 function Alert(props) {
+    const capitalize = (word) => {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+    };
+
     return (
-        <>
-            <div className="alert alert-warning alert-dismissible fade show" role="alert">
-                <strong>{props.text}</strong> You should check in on some of those fields below.
+// using && operator because default value of 'alert' is NULL 
+        props.alert && (
+            <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+                <strong>{capitalize(props.alert.type)} ! </strong>
+                {props.alert.msg}
                 <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
             </div>
-        </>
+        )
     );
 }
 
