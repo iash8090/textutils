1b5b57b1d5ca5522b70cc3e7e9a032e9b38ed5ef
ASH <25776660+iash8090@users.noreply.github.com>
2023-08-29 16:43:42 +0530

    Update Alert.js

--- src/Componants/Alert.js
+++ src/Componants/Alert.js
@@ -6,14 +6,16 @@ function Alert(props) {
     };
 
     return (
-// using && operator because default value of 'alert' is NULL 
-        props.alert && (
+        <div style={{height: '50px'}}>
+{/* // using && operator because default value of 'alert' is NULL */}
+        { props.alert && (
             <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
                 <strong>{capitalize(props.alert.type)} ! </strong>
                 {props.alert.msg}
                 <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
             </div>
-        )
+        )}
+        </div>
     );
 }
 
